2a00cd48d311f13c79bcd8191610a821825bb899
jigs074 <90673637+jigs074@users.noreply.github.com>
2022-10-04 12:17:17 -0400

    Method for calulcating fees and some modifications have been made in the attributes of class . Still some issues in the tution fees calculator which needs to be resolved.

--- Student management system/Source.cpp
+++ Student management system/Source.cpp
@@ -11,20 +11,78 @@ class student {
 	string firstName; 
 	string lastName; 
 	int studentId; 
-	long int ProgramCode; 
+	 
+	int grade; 
 
 public: 
 
 	void set_data() {
+		 student s; 
+
+
+		cout << "Enter the grade of a student you are looking for " << endl; 
+		cin >> grade; 
+		
+		if (grade == 10) {
+			 
+
+			studentId = rand() % ((30 - 1)+1)  ; 
+			cout << studentId << endl; 
+
+		}
+		if (grade == 11) {
+			//int Grade_11id = 0; 
+			studentId = rand() % ((60 - 31) + 31); 
+			cout << studentId << endl; 
+
+		}
+		if (grade == 12) {
+			//int Grade_12i = 0; 
+			studentId = rand() % ((90 - 61) + 61); 
+			cout << studentId << endl; 
+
+		}
+		if (grade != 10 && grade != 11 && grade != 12) {
+			cout << "Invalid input , This program is only for high school students" << endl;
+
+		}
 		cout << "Student's first name :" << endl;
 		cin >> firstName; 
 		cout << "Student last name : " << endl; 
 		cin >> lastName; 
-		cout << "Student Id :" << endl;
-		cin >> studentId; 
-		cout << "Program code : "; 
-          cin >> ProgramCode; 
+		
 	}
+	// fees will increase to 5 % every year 
+	// grade 10 will get student id  from 1 to 30
+	// grade 11 will get student id from  31 to 60 
+	// grade 12 student will be having id from 61 to 90
+	// grade 10 tution fees - $ 3000 
+	// grade 11 tution fees - $ 3500 
+	// grade 12 tution fees- $ 4000 
+
+	void  fees() {
+		student s; 
+
+		long int Tutionfees =0 ; 
+		
+		cout << "Fees Information for St Mary's high school :  " << endl; 
+		if (s.studentId <= 30) {
+			Tutionfees = 3000; 
+			cout << "Fees for grade 10 is :  " << Tutionfees << endl; 
+
+	   }
+		if (s.studentId <= 60 || s.studentId >= 30) {
+			Tutionfees = 3500; 
+			cout << "Fees for grade 11 is :  " << Tutionfees << endl; 
+
+		}
+		if (s.studentId <= 90 || s.studentId >= 60) {
+			Tutionfees = 4000; 
+			cout << "Fees for grade 12 is : " << Tutionfees << endl; 
+
+		}
+	}
+
 };
 class Academics : public  student {
 	// after getting marks of student program should be able to calculate the average percentage 
@@ -74,10 +132,9 @@ public :
 };
 
 void main() {
-	//student s; 
-	Academics a;
-	// a.set_data(); 
-	a.get_AverageMarks(); 
+	student s; 
+	s.set_data(); 
+	s.fees(); 
 
 
 
